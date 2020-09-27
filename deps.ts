1e2217ffd5f14cb187b0f2cb70bb56ebd4ca0e25
Luca Casonato <luca.casonato@antipy.com>
2020-09-27 08:37:21 -0700

    chore: update Deno to 1.4.2 and std to 0.71.0 (#5)

--- deps.ts
+++ deps.ts
@@ -1,6 +1,6 @@
 export { hmac } from "https://denopkg.com/chiefbiiko/hmac@v1.0.2/mod.ts";
 
-import { createHash } from "https://deno.land/std@0.69.0/hash/mod.ts";
+import { createHash } from "https://deno.land/std@0.71.0/hash/mod.ts";
 export function sha256Hex(data: string | Uint8Array): string {
   const hasher = createHash("sha256");
   hasher.update(data);
