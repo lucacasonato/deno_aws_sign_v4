4a940a35bd4b692801eccca636b8e2c40bcd623a
Daniel Lamando <dan@danopia.net>
2021-05-22 14:53:33 +0200

    Make sha256.ts usage more consistent

--- deps.ts
+++ deps.ts
@@ -1,14 +1,12 @@
-import { createHash } from "https://deno.land/std@0.95.0/hash/mod.ts";
-export function sha256Hex(data: string | Uint8Array): string {
-  const hasher = createHash("sha256");
-  hasher.update(data);
-  return hasher.toString("hex");
+import { HmacSha256, Sha256 } from "https://deno.land/std@0.95.0/hash/sha256.ts";
+import type { Message } from "https://deno.land/std@0.95.0/hash/sha256.ts";
+
+export function sha256(data: Message): Sha256 {
+  const hasher = new Sha256();
+  return hasher.update(data);
 }
 
-import { HmacSha256 } from "https://deno.land/std@0.95.0/hash/sha256.ts";
-import type { Message } from "https://deno.land/std@0.95.0/hash/sha256.ts";
 export function hmacSha256(key: Message, data: Message): HmacSha256 {
   const hasher = new HmacSha256(key);
-  hasher.update(data);
-  return hasher;
+  return hasher.update(data);
 }
